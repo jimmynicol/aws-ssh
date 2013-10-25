679f927077af1b751dc954d7365ef4e89c204ee5
jimmynicol <james@fundly.com>
2013-10-24 17:02:46 -0700

    Started buildling the AWS api interactions.

--- bin/cli.js
+++ bin/cli.js
@@ -17,6 +17,7 @@ program
   .option('-v, --version', 'Current aws-ssh version')
   .parse(process.argv);
 
+// handle initializing the tool, asking for the key and secret
 if (program.init){
   var schema, config;
 
@@ -36,37 +37,59 @@ if (program.init){
       secretAccessKey: {
         description: 'Secret Access Key (secretAccessKey):',
         required: true
+      },
+      region: {
+        description: 'Assign the region',
+        required: true,
+        default: 'us-east-1'
       }
     }
   };
 
   prompt.get(schema, function(err, result){
+    console.log(result);
+
     if (fs.existsSync(configFile)){
       config = JSON.parse(fs.readFileSync(configFile));
       config.accessKeyId = result.accessKeyId;
       config.secretAccessKey = result.secretAccessKey;
-      fs.writeFileSync(configFile, JSON.stringify(config));
+      config.region = result.region;
+      fs.writeFileSync(
+        configFile,
+        JSON.stringify(config),
+        'UTF-8',
+        {'flags': 'w+'}
+      );
     } else {
-      fs.writeFileSync(configFile, JSON.stringify(result));
+      fs.writeFileSync(
+        configFile,
+        JSON.stringify(result),
+        'UTF-8',
+        {'flags': 'w+'}
+      );
     }
 
     console.log('✎', configFile.green, 'written!\n\n');
   });
 }
 
+
+// print out the config as listed
 if (program.config){
+  // make sure we have a config in place
   if (fs.existsSync(configFile)){
     config = JSON.parse(fs.readFileSync(configFile));
-
-    console.log('\n★  Your config settings are:\n'.yellow);
-    console.log('accessKeyId:     ', config.accessKeyId.grey.bold);
-    console.log('secretAccessKey: ', config.secretAccessKey.grey.bold);
-    console.log('');
   } else {
     console.log('Please run `aws-ssh init` to build your config settings'.red);
   }
+
+  console.log('\n★  Your config settings are:\n'.yellow);
+  console.log('accessKeyId:     ', config.accessKeyId.grey.bold);
+  console.log('secretAccessKey: ', config.secretAccessKey.grey.bold);
+  console.log('region:          ', config.region.grey.bold);
+  console.log('');
 }
 
 if (program.args.length > 0){
-
+  require('../lib/instances').listInstances(program.args[0]);
 }
